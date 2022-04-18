3f12a9b64a1d37e89f85c4a01138bf3aa7f12c3c
erwin <erwinis001@gmail,com>
2022-04-18 10:19:38 +0800

    Initial Commit

---
+++ Shared/number_mergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  number_mergeApp.swift
+//  Shared
+//
+//  Created by  Erwin on 2022/4/18.
+//
+
+import SwiftUI
+
+@main
+struct number_mergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
